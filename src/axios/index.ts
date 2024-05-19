98dadddacdea7e38b888d57051681cda3ea9a1ac
Vladyslav Yatsenko <107263996+Miksirist@users.noreply.github.com>
2024-05-19 17:50:58 +0300

    added verification for thisday

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://api.openweathermap.org/data/2.5',
 });
 
 api.interceptors.request.use((config) => {
-  config.url = `${config.url}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+  config.url = `${config.url}&units=metric&appid=6a4623f9086a9971320253741df1a21c`;
   return config;
 });
 
