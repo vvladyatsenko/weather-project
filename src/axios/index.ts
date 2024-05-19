5a500050cf6df9d8b5d0fa51a49608b1715311cb
Vladyslav Yatsenko <107263996+Miksirist@users.noreply.github.com>
2024-05-19 17:48:00 +0300

    added verification for thisday

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -5,8 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  config.url =
-    config.url + '&units=metric' + '&appid=' + process.env.REACT_APP_API_KEY;
+  config.url = `${config.url}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
   return config;
 });
 
