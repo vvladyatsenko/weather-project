0a5f88713850397c92af3544c002c89cb5738798
Vladyslav Yatsenko <107263996+Miksirist@users.noreply.github.com>
2024-05-19 15:57:05 +0300

    added options for weather and other

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
-import { Days } from './components/Days/Days';
+import { useEffect } from 'react';
+import { useCustomDispatch, useCustomSelector } from '../../hooks/store';
+import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather';
+import { selectCurrentWeatherData } from '../../store/selectors';
+import { Header } from '../../shared/Header/Header';
 import style from './Home.module.scss';
 import { ThisDay } from './components/ThisDay/ThisDay';
 import { ThisDayInfo } from './components/ThisDayInfo/ThisDayInfo';
-import { useCustomDispatch } from '../../hooks/store';
-import { useEffect } from 'react';
-import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather';
-import { useCustomSelector } from '../../hooks/store';
-import { selectCurrentWeatherData } from '../../store/selectors';
+import { Days } from './components/Days/Days';
 
 interface Props {}
 
@@ -15,14 +15,21 @@ export const Home = (props: Props) => {
   const { weather } = useCustomSelector(selectCurrentWeatherData);
 
   useEffect(() => {
-    dispatch(fetchCurrentWeather('paris'));
-  }, []);
+    dispatch(fetchCurrentWeather('Kharkiv'));
+  }, [dispatch]);
+
+  const updateWeather = (city: string) => {
+    dispatch(fetchCurrentWeather(city));
+  };
 
   return (
     <div className={style.home}>
+      <Header onCityChange={updateWeather} />
       <div className={style.wrapper}>
-        <ThisDay weather={weather} />
-        <ThisDayInfo />
+        <>
+          <ThisDay weather={weather} />
+          <ThisDayInfo weather={weather} />
+        </>
       </div>
       <Days />
     </div>
--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -1,12 +1,77 @@
 import { GlobalSvgSelector } from '../../../assets/icons/global/GlobalSvgSelector';
 import style from './ThisDay.module.scss';
 import { Weather } from '../../../../store/types/types';
+import moment from 'moment';
 
 interface Props {
   weather: Weather;
 }
 
 export const ThisDay = ({ weather }: Props) => {
+  const getWeatherIcon = (icon: string) => {
+    switch (icon) {
+      case '01d': // Ясно (день)
+      case '01n': // Ясно (ночь)
+        return 'sun';
+      case '02d': // Небольшая облачность (день)
+      case '02n': // Небольшая облачность (ночь)
+        return 'mainly_cloudy';
+      case '03d': // Облачно
+      case '03n': // Облачно
+      case '04d': // Сильная облачность
+      case '04n': // Сильная облачность
+        return 'mainly_cloudy';
+      case '09d': // Мелкий дождь (день)
+      case '09n': // Мелкий дождь (ночь)
+        return 'small_rain';
+      case '10d': // Дождь (день)
+      case '10n': // Дождь (ночь)
+        return 'rain';
+      case '11d': // Гроза (день)
+      case '11n': // Гроза (ночь)
+        return 'storm';
+      case '13d': // Снег (день)
+      case '13n': // Снег (ночь)
+        return 'snow';
+      case '50d': // Туман (день)
+      case '50n': // Туман (ночь)
+        return 'mist';
+      default:
+        return 'sun';
+    }
+  };
+
+  const iconId = getWeatherIcon(weather.weather[0].icon);
+  const currentTime = moment().format('HH:mm');
+  const currentDate = moment().format('DD.MM.YYYY');
+
+  const cityName = (name: string) => {
+    switch (name) {
+      case 'Kharkiv':
+        return 'Харків';
+      case 'Kyiv':
+        return 'Київ';
+      case 'Odessa':
+        return 'Одеса';
+      case 'Dnipro':
+        return 'Дніпро';
+      case 'Zaporizhzhia':
+        return 'Запоріжжя';
+      case 'Lviv':
+        return 'Львів';
+      case 'Mykolaiv':
+        return 'Миколаїв';
+      case 'Vinnytsia':
+        return 'Вінниця';
+      case 'Kherson':
+        return 'Херсон';
+      case 'Chernihiv':
+        return 'Чернігів';
+      default:
+        return name;
+    }
+  };
+
   return (
     <div className={style.this__day}>
       <div className={style.top__block}>
@@ -14,16 +79,19 @@ export const ThisDay = ({ weather }: Props) => {
           <div className={style.this__temp}>
             {Math.floor(weather.main.temp)}°
           </div>
-          <div className={style.this__day_name}>Today</div>
+          <div className={style.this__day_name}>Сьогодні</div>
         </div>
-        <GlobalSvgSelector id={'sun'} />
+        <GlobalSvgSelector id={iconId} />
       </div>
       <div className={style.bottom__block}>
         <div className={style.this__time}>
-          Time: <span>21:55</span>
+          Час: <span>{currentTime}</span>
+        </div>
+        <div className={style.this__date}>
+          Дата: <span>{currentDate}</span>
         </div>
         <div className={style.this__city}>
-          <span>Kharkov</span>
+          Місто: <span>{cityName(weather.name)}</span>
         </div>
       </div>
     </div>
