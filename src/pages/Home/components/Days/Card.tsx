5077f906f9b5d3cbbf7cb05cfe2267575776aa9c
Vladyslav Yatsenko <107263996+Miksirist@users.noreply.github.com>
2024-05-19 17:10:38 +0300

    deploy

--- src/pages/Home/components/Days/Card.tsx
+++ src/pages/Home/components/Days/Card.tsx
@@ -7,17 +7,18 @@ interface Props {
 }
 
 export const Card = ({ day }: Props) => {
-  const { day: dayName, day_info, icon_id, temp_day, temp_night } = day;
+  const { day: dayName, day_info, icon_id, temp_day, temp_night, info } = day;
 
   return (
     <div className={style.card}>
       <div className={style.day}>{dayName}</div>
-      <div className={style.day__info}>{day.day_info}</div>
+      <div className={style.day__info}>{day_info}</div>
       <div className={style.img}>
-        <GlobalSvgSelector id={day.icon_id} />
+        <GlobalSvgSelector id={icon_id} />
       </div>
-      <div className={style.temp__day}>{day.temp_day}</div>
-      <div className={style.temp__night}>{day.temp_night}</div>
+      <div className={style.temp__day}>{temp_day}</div>
+      <div className={style.temp__night}>{temp_night}</div>
+      <div className={style.info}>{info}</div>
     </div>
   );
 };
